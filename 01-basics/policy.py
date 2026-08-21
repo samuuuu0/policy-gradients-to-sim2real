79e0723dd81150ef35676af1b86e116ba2bddb54
samuuuu0 <samueletommasi29@gmail.com>
2026-08-21 23:29:09 +0200

    implement policy actor network

---
+++ 01-basics/policy.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.distributions import Normal
+
+
+class Actor(nn.Module):
+    def __init__(
+        self,
+        state_space: int,
+        action_space: int,
+        hidden_dim: int = 64,
+        init_sigma: float = 0.5
+    ):
+        super().__init__()
+
+        self.state_space = state_space
+        self.action_space = action_space
+
+        self.actor_net = nn.Sequential(
+            nn.Linear(self.state_space, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, self.action_space)
+        )
+
+        self.sigma = nn.Parameter(
+            torch.full((self.action_space,), init_sigma, dtype=torch.float32)
+        )
+
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0.0, std=0.1)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, state: torch.Tensor) -> Normal:
+        action_mean = self.actor_net(state)
+        sigma = F.softmax(self.sigma)
+        return Normal(loc=action_mean, scale=sigma)
