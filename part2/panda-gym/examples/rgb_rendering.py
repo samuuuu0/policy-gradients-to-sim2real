f80fa94c8147075ed39ceeb729db772692a83b72
samuuuu0 <samueletommasi29@gmail.com>
2026-09-13 03:34:56 +0200

    setup project for part2

---
+++ part2/panda-gym/examples/rgb_rendering.py
@@ -0,0 +1,22 @@
+import gymnasium as gym
+from numpngw import write_apng  # pip install numpngw
+
+env = gym.make("PandaStack-v3", render_mode="rgb_array")
+images = []
+
+
+observation, info = env.reset()
+images.append(env.render())
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    images.append(env.render())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+        images.append(env.render())
+
+env.close()
+
+write_apng("stack.png", images, delay=40)
