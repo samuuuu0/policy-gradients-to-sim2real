50af18ba355b8e8dd1f354c1d37ef3c1d165ae80
samuuuu0 <samueletommasi29@gmail.com>
2026-09-15 17:35:40 +0200

    train and tune sac for evaluation

--- part2/train.py
+++ part2/train.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+import yaml
 
 import gymnasium as gym
 import panda_gym
@@ -12,12 +13,9 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Train SAC/PPO on PandaPush-v3")
-    parser.add_argument("--algo", type=str, choices=["ppo", "sac"])
-    parser.add_argument(
-        "--env-type", type=str, default="source", choices=["source", "target"]
-    )
+    parser.add_argument("--config", type=str, default="../configs/part2_sac.yaml")
+    parser.add_argument("--env-type", type=str, choices=["source", "target"])
     parser.add_argument("--sampling-strategy", type=str, choices=["none", "udr", "adr"])
-    parser.add_argument("--timesteps", type=int, default=500_000)
     parser.add_argument("--seed", type=int, default=42)
     return parser.parse_args()
 
@@ -25,15 +23,18 @@ def parse_args() -> argparse.Namespace:
 def main():
     args = parse_args()
 
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+
     log_dir = Path("logs/part2")
     model_dir = Path("models/part2")
     log_dir.mkdir(parents=True, exist_ok=True)
     model_dir.mkdir(parents=True, exist_ok=True)
 
-    exp_name = f"{args.algo}_{args.env_type}_{args.sampling_strategy}_s{args.seed}"
+    exp_name = f"{config['algo']}_{args.env_type}_{args.sampling_strategy}_s{args.seed}"
     print(f"Starting Training: {exp_name}")
 
-    n_envs = 8
+    n_envs = config.get("n_envs", 8)
     env = make_vec_env(
         "PandaPush-v3",
         n_envs=n_envs,
@@ -42,31 +43,37 @@ def main():
         env_kwargs={"type": args.env_type, "reward_type": "dense"},
     )
 
-    # env = gym.make("PandaPush-v3", type=args.env_type, reward_type="dense")
-    # env = Monitor(env)
-
     # TODO: domain randomization wrapper (task 6)
 
     eval_env = gym.make("PandaPush-v3", type=args.env_type, reward_type="dense")
     eval_env = Monitor(eval_env)
 
-    if args.algo == "ppo":
-        model = PPO("MultiInputPolicy", env, verbose=1, seed=args.seed, device="cpu")
-    elif args.algo == "sac":
-        model = SAC("MultiInputPolicy", env, verbose=1, seed=args.seed, device="cpu")
-    else:
-        raise ValueError(f"Algorithm not found {args.algo}")
+    hyperparams = config.get("hyperparams", {})
 
+    if config['algo'] == "ppo":
+        model = PPO("MultiInputPolicy", env, verbose=1, seed=args.seed, device="cpu")
+    elif config['algo'] == "sac":
+        model = SAC(
+            "MultiInputPolicy", 
+            env, 
+            verbose=1, 
+            seed=args.seed,
+            **hyperparams
+        )
+    else:
+        raise ValueError(f"Algorithm not found {config['algo']}")
+
+    eval_freq = max(10_000 // n_envs, 1)
     eval_callback = EvalCallback(
         eval_env,
         best_model_save_path=str(model_dir / exp_name),
         log_path=str(log_dir / exp_name),
-        eval_freq=10_000,
+        eval_freq=eval_freq,
         deterministic=True,
         render=False,
     )
 
-    model.learn(total_timesteps=args.timesteps, callback=eval_callback)
+    model.learn(total_timesteps=config["timesteps"], callback=eval_callback)
 
     print(
         f"Training finished. Best model saved in {model_dir / exp_name / 'best_model.zip'}"
